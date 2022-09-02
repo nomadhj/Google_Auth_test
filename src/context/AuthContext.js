6e0ed6273bdc7e088b59b7259d990ec490a9ab42
hyeonjoo KIM <machinahj@gmail.com>
2022-09-02 19:06:08 +0900

    first commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,51 @@
+import { createContext, useState, useEffect } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext({
+  user: {},
+  isLoggedIn: false,
+  logIn: () => {},
+  logOut: () => {},
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const logIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  const authContext = {
+    user,
+    isLoggedIn: !!user?.accessToken,
+    logIn,
+    logOut,
+  };
+
+  useEffect(() => {
+    const stateChange = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      stateChange();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
